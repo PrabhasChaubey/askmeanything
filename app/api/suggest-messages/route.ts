78075ea0373dabed17fa90f920135bfde0b9ccb5
Prabhas Chaubey <prabhaschaubey46@gmail.com>
2026-01-24 16:44:48 +0530

    completed suggest messages

--- app/api/suggest-messages/route.ts
+++ app/api/suggest-messages/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: Request) {
       apiKey: process.env.OPENAI_API_KEY,
     });
 
-    const prompt = "..."; // your prompt
+    const prompt =  "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment."; // your prompt
 
     const response = await openai.completions.create({
       model: 'gpt-3.5-turbo-instruct',
@@ -28,6 +28,14 @@ export async function POST(req: Request) {
       },
     });
   } catch (error) {
-    // error handling
+    if(error instanceof OpenAI.APIError){
+        const{name,status,headers,message}=error;
+        return NextResponse.json({
+            name,status,headers,message 
+        },{status})
+    }else{
+        console.log("An unexpected error occured: ",error)
+        throw error;
+    }
   }
 }
