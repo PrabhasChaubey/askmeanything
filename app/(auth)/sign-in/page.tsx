00bc3cf4d63b4a1fad391c6ba9ac5eed3239cfea
Prabhas Chaubey <prabhaschaubey46@gmail.com>
2026-01-28 23:32:08 +0530

    done debouncedValue

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,20 +1,55 @@
 'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
+import zodResolver from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as z  from 'zod'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import {useDebounceValue} from 'usehooks-ts'
+import { toast } from "sonner"
+import { useRouter } from 'next/navigation'
+import { Sign } from 'crypto'
+import { signUpSchema } from '@/src/schemas/signUpSchema'
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
+
+function page() {
+  const [username,setUsername] = useState('')
+  const [usernameMessage,setUsernameMessage] = useState('')
+  const [isChecingUsername,setIsChecingUsername] = useState(false)
+  const [isSubmitting,setIsSubmitting] = useState(false)
+
+  const debouncedUsername=useDebounceValue(username,300)
+
+  const router=useRouter();
+
+  //zod implementation
+  const form=useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues:{
+      username:'',
+      email:'',
+      password:''
+    }
+  })
+
+  useEffect(()=>{
+    const checkUsernameUnique=async()=>{
+      if(debouncedUsername){
+        setIsChecingUsername(true)
+        setUsernameMessage('')
+
+        try {
+          
+        } catch (error) {
+          
+        }
+      }
+    }
   }
+  ,[debouncedUsername])
+
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <div>page</div>
   )
 }
+
+export default page
