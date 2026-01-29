15275b051a1481476bf6c77caabb128e2ab51919
Prabhas Chaubey <prabhaschaubey46@gmail.com>
2026-01-30 00:17:50 +0530

    completed Navbar and MessageCard

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import './globals.css'
-import AuthProvider from "@/src/context/AuthProvider";
-import { Toaster } from '@/components/ui/sonner'
 import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
@@ -27,17 +24,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
+      
 
         <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <Navbar/>
           {children}
-        <Toaster />
+        
         </body>
 
-      </AuthProvider>
+      
       
     </html>
   );
